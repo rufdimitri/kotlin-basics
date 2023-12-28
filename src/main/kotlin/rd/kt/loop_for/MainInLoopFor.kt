ce69fe5845f49c05788bc57a51dad34d5d8fa059
rufdimitri <ruf.dimitri.de@gmail.com>
2023-12-28 12:10:51 +0100

    add foreach example with list

--- src/main/kotlin/rd/kt/loop_for/MainInLoopFor.kt
+++ src/main/kotlin/rd/kt/loop_for/MainInLoopFor.kt
@@ -1,14 +1,23 @@
 package rd.kt.loop_for
 
 fun main() {
-    for1();
-
+    for1()
+    for2()
 }
 
 fun for1() {
     println("for (x in 0..9) {")
     for (x in 0..9) {
-        println("  x = $x");
+        println("  x = $x")
+    }
+    println("}")
+}
+
+fun for2() {
+    val autos = listOf("BMW", "Mercedes", "Audi")
+    println("for (auto in autos) {")
+    for (auto in autos) {
+        println("  $auto")
     }
     println("}")
 }
